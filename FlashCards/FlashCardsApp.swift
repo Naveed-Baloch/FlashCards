1942cb40d01acdfb61d48a297bfb0c92e5ea22b2
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-10-08 23:17:17 +0500

    Initial Commit

---
+++ FlashCards/FlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardsApp.swift
+//  FlashCards
+//
+//  Created by Naveed on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
